a616f2830d17ce414836f3eeb194e3f698385d79
joaopsb123 <joaopsb12223@gmail.com>
2026-02-21 16:38:07 +0000

    Replace MongoDB with Firebase for user data

--- api/user.py
+++ api/user.py
@@ -1,9 +1,18 @@
 from http.server import BaseHTTPRequestHandler
 import json
-from pymongo import MongoClient
-from datetime import datetime
+import firebase_admin
+from firebase_admin import db
+import os
 
-MONGODB_URI = "mongodb+srv://joaopsb6890_db_user:BcOi8oG5uQwcuLBl@cluster0.xdlwmli.mongodb.net/bot_saldo?retryWrites=true&w=majority"
+# Configuração do Firebase (sem credenciais complexas)
+FIREBASE_CONFIG = {
+    "databaseURL": "https://bot-discord-4d74d-default-rtdb.firebaseio.com/",
+    "projectId": "bot-discord-4d74d"
+}
+
+# Inicializar Firebase (uma vez só)
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(options=FIREBASE_CONFIG)
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -14,14 +23,10 @@ class handler(BaseHTTPRequestHandler):
         
         try:
             user_id = self.path.split('/')[-1]
-            client = MongoClient(MONGODB_URI)
-            db = client['bot_saldo']
-            user = db['users'].find_one({'user_id': user_id})
+            user_ref = db.reference(f'/users/{user_id}')
+            user = user_ref.get()
             
             if user:
-                user['_id'] = str(user['_id'])
-                if user.get('last_daily'):
-                    user['last_daily'] = str(user['last_daily'])
                 self.wfile.write(json.dumps(user).encode())
             else:
                 self.wfile.write(json.dumps({
@@ -33,3 +38,10 @@ class handler(BaseHTTPRequestHandler):
                 }).encode())
         except Exception as e:
             self.wfile.write(json.dumps({'error': str(e)}).encode())
+    
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.end_headers()
